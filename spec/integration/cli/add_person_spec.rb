9a405108eb7b8358afc1a61b2dfa60a28f7c2613
Sebastian Sangervasi <s.ebastian.sangervasi@gmail.com>
2019-05-19 23:17:22 -0700

    ok fine i will exec stuff

--- spec/integration/cli/add_person_spec.rb
+++ spec/integration/cli/add_person_spec.rb
@@ -4,11 +4,8 @@ require 'spec_helper'
 require 'spec_helpers/cli'
 
 RSpec.describe 'fam add person' do
-  include_context 'cli'
-
   it 'works' do
-    out = StringIO.new
-    cli.call(arguments: %w[fam add person], out: out)
-    expect(out.string).to include('fam')
+    result = exec_fam(%w[add person])
+    expect(result.output).to include('fam')
   end
 end
--- spec/spec_helpers/cli.rb
+++ spec/spec_helpers/cli.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
+require 'open3'
+
 require 'fam/cli'
 
-RSpec.shared_context 'cli' do
-  let(:cli) { Hanami::CLI.new(Fam::CLI) }
+ExecResult = Struct.new(:output, :error, :status)
+
+def exec_fam(fam_args)
+  exec_args = %w[fam] + fam_args
+  # Crazily, this is the best way to exec.
+  ExecResult.new(*Open3.capture3(*exec_args))
 end
